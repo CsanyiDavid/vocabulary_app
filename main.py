12b1b046f998ae3532cda9c7cd6acfd96d4f589e
David Csanyi <csanyidavid2000@gmail.com>
2024-07-29 14:47:35 +0200

    main and word class

---
+++ main.py
@@ -0,0 +1,16 @@
+
+class Word:
+    def __init__(self, word):
+        self.__word = word
+        #translation
+        #status
+        #meaning
+        #context
+
+
+def main():
+    word_list = []
+
+
+if __name__ == "__main__":
+    main()
